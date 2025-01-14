e5a359b31443911ed783444cfc0ff2a8a31d4bf2
ericthedev7 <ericthedev97@gmail.com>
2025-01-15 02:03:52 +0700

    add pages

---
+++ src/views/Monastery.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t, locale } = useI18n();
+
+</script>
+
+<template>
+  <!-- Hero Section -->
+  <div class="relative min-h-screen flex items-center justify-center">
+    <div class="relative container mx-auto px-4 text-center">
+      <h1
+        class="text-xl md:text-5xl font-bold mb-6 animate-slide-in"
+      >
+        {{ t("bio.title") }}
+        Monastery
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
